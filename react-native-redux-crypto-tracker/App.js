dddae85e8e92b49eb87b56ef54e5d420b2f8908e
Balbina <balbinasantana@gmail.com>
2018-03-21 15:02:36 -0600

    Added header component

--- react-native-redux-crypto-tracker/App.js
+++ react-native-redux-crypto-tracker/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Header } from './src/components';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+      <Header />
         <Text>On wednesdays we wear pink.</Text>
       </View>
     );
